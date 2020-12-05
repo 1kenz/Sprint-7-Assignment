506aca01299108e6e2ba2f715edd47a28eb82439
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-05 22:53:25 +0300

    feat: add navbar from bootsnipp

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const HomeRouter = require("./routes/HomeRouter");
 
 const RegisterRouter = require("./routes/RegisterRouter");
 
+const LoginRouter = require("./routes/LoginRouter");
+
 const port = process.env.port || 5000;
 
 const app = express();
@@ -14,6 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", HomeRouter);
 app.get("/register", RegisterRouter);
+app.get("/login", LoginRouter);
 
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
